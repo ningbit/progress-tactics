a64fdf8613073a553cc583e934c9a2081d0a0461
Ning Yap <ning.yap@gmail.com>
2013-10-13 19:10:48 -0400

    root set to users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ProgressTactics::Application.routes.draw do
   resources :users
 
+  root :to => 'users#index'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
