5b2f1c822e0b6d322df5077fa4a9d4f4dbec8a4e
Ning Yap <ning.yap@gmail.com>
2013-10-13 16:38:44 -0400

    load in image assets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProgressTactics::Application.config.secret_token = '30ac908f1a02867b1c37a388b3d8cc34e032b116508652ab8b6004ccdc0012ae85e1ab1bbc09ebfaec6415d1232d9733e1deed56dfb5c48a31ccd85776cdb852'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ProgressTactics::Application.config.session_store :cookie_store, key: '_progress-tactics_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ProgressTactics::Application.config.session_store :active_record_store
